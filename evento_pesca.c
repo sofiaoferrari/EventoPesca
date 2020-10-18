48d67cd786d5938f6ca8ce1b831282117c38fda5
sofiaoferrari <soferrari@fi.uba.ar>
2020-10-18 13:09:49 -0300

    Modifico .c

--- evento_pesca.c
+++ evento_pesca.c
@@ -21,28 +21,39 @@ typedef struct arrecife{
 } arrecife_t;
 */
 
-#define FORMATO [^%s];
+#define FORMATO_LECTURA "%[^;];%i;%i;%[^\n]\n"
 
 arrecife_t* crear_arrecife(const char* ruta_archivo[]) {
     FILE* archivo = fopen(*ruta_archivo, "r");
+    arrecife_t* nuevo_poke = malloc(sizeof(arrecife_t));
 
     if (archivo == NULL) {
-//        printf("ERROR de lectura.");
+        printf("ERROR de lectura.");
+        return -1;
     }
 
-    arrecife_t* nuevo_pokemon = malloc(sizeof(arrecife_t));
+    int leidos = fscanf(archivo, FORMATO_LECTURA,
+                        nuevo_poke->pokemon->especie,
+                        &nuevo_poke->pokemon->peso,
+                        &nuevo_poke->pokemon->velocidad,
+                        nuevo_poke->pokemon->color);
 
-    int feof(FILE* archivo);
-/*
-    while (!feof(archivo)){
-        fread();
-        fscanf();
-    }*/
+//    int feof(FILE* archivo);
+//    fread();
+//    fscanf();
 
-    strcpy(nuevo_pokemon->pokemon, &pokemon);
-    nuevo_pokemon->cantidad_pokemon, &cantidad_pokemon;
+    while (leidos != EOF){
+        fprintf(archivo, FORMATO_LECTURA,
+                nuevo_poke->pokemon->especie,
+                &nuevo_poke->pokemon->peso,
+                &nuevo_poke->pokemon->velocidad,
+                nuevo_poke->pokemon->color);
+    }
+
+// strcpy(nuevo_poke->pokemon, &pokemon);
+    nuevo_poke->cantidad_pokemon, &leidos;
     
     fclose(archivo);
-    free(nuevo_pokemon);
+    free(nuevo_poke);
     return *arrecife_t;   //puntero arrecife
 }
