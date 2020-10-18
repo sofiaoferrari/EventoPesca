8a04ec34d83bacaa352901a95a8a19e99f9f5916
sofiaoferrari <soferrari@fi.uba.ar>
2020-10-18 01:33:54 -0300

    cambios insignificantes :(

--- evento_pesca.c
+++ evento_pesca.c
@@ -22,23 +22,25 @@ typedef struct arrecife{
 */
 
 arrecife_t* crear_arrecife(const char* ruta_archivo[]) {
-    FILE* archivo = fopen(ruta_archivo, "r");
+    FILE* archivo = fopen(*ruta_archivo, "r");
 
-    if (archivo == NULL) {ERROR}
+    if (archivo == NULL) {
 //        printf("ERROR de lectura.");
-    arrecife_t* nuevo_pokemon = malloc(sizeof(arrecife_t);
+    }
+
+    arrecife_t* nuevo_pokemon = malloc(sizeof(arrecife_t));
 
     int feof(FILE* archivo);
-
+/*
     while (!feof(archivo)){
         fread();
         fscanf();
-    }
+    }*/
 
-    strcpy(nuevo_pokemon->pokemon, *pokemon);
-    nuevo_pokemon->cantidad_pokemon, cantidad_pokemon;
+    strcpy(nuevo_pokemon->pokemon, &pokemon);
+    nuevo_pokemon->cantidad_pokemon, &cantidad_pokemon;
     
     fclose(archivo);
     free(nuevo_pokemon);
-    return nuevo_pokemon;//puntero
+    return *arrecife_t;   //puntero
 }
