a3940a950b9e84ea958531629121e5452b8c5850
sofiaoferrari <soferrari@fi.uba.ar>
2020-10-27 21:35:23 -0300

    empezando

--- main.c
+++ main.c
@@ -2,10 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+bool poke_especie_especifica(){
+    return encontrado;
+}
+
+bool pokefofos(){
+    encontrado = false;
+    
+    return encontrado;
+}
+
 int main() {
     const char ARCHIVO[] = "arrecife.txt";
     arrecife_t* simulacion_arrecife = crear_arrecife(ARCHIVO); 
-
+    acuario_t* simulacion_acuario = crear_acuario();
+    //Trasladar y listar pokes n-veces
+    //Guardar los pokemon del acuario
+    //liberar memoria
     free(simulacion_arrecife);
     return 0;
 }
